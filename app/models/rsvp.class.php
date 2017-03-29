42d8cd4f3709e5b2175da2cbe7f869635b60303d
proggR <brandonthorn@sourced.fm>
2017-03-29 09:46:43 -0400

    Added attending field

--- app/models/rsvp.class.php
+++ app/models/rsvp.class.php
@@ -8,6 +8,7 @@ class RSVP extends Model{
         parent::__construct();
         $this->name = '';
         $this->phone = '';
+        $this->attending = '';
         $this->names_attending = '';
         $this->dietary_notes = '';
         $this->song_suggestions = '';
@@ -15,7 +16,8 @@ class RSVP extends Model{
         $this->validation_rules = array(
             'name' => array('rule'=>'string,required','fieldname'=>'Fullname'),
             'phone' => array('rule'=>'string,required','fieldname'=>'Phone Number'),
-            'names_attending' => array('rule'=>'string,required','fieldname'=>'Names of Attendees'),
+            'attending' => array('rule'=>'string,required','fieldname'=>'Phone Number'),
+            'names_attending' => array('rule'=>'string','fieldname'=>'Names of Attendees'),
             'dietary_notes' => array('rule'=>'string','fieldname'=>'Dietary Restrictions'),
             'song_suggestions' => array('rule'=>'string','fieldname'=>'Song Suggestions'),
         );        
@@ -28,6 +30,7 @@ class RSVP extends Model{
         $stmt->query = 'rsvps';
         $stmt->bind('name',$this->name,'string');
         $stmt->bind('phone',$this->phone,'string');
+        $stmt->bind('attending',$this->attending,'string');
         $stmt->bind('names_attending',$this->names_attending,'string');
         $stmt->bind('dietary_notes',$this->dietary_notes,'string');
         $stmt->bind('song_suggestions',$this->song_suggestions,'string');
